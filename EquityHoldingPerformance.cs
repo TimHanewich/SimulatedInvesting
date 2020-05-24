308302c2be823f7cd7d58bbab145bd2395684b9b
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-05-24 16:22:00 -0700

    Put 'on-hold' comment in the EquityHoldingPerformance class.
    This will be put on hold until a fifo or lifo method for buying + selling stocks is created

--- EquityHoldingPerformance.cs
+++ EquityHoldingPerformance.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace SimulatedInvesting
 {
+    //This represents gain/loss performance for an individual stock.
+    //Placed this on hold as of 5/24/2020 while a FIFO/LIFO method for recording stock purchases and sales is created in the core SimulatedPortfolio class.
     public class EquityHoldingPerformance
     {
         public string Symbol {get; set;}
